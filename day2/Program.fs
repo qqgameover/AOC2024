6ca29a07461d5375d7a81910e5f58d9526e19140
Kasper <kl@avilo.no>
2024-12-02 11:46:38 +0100

    Day 2

---
+++ day2/Program.fs
@@ -0,0 +1,36 @@
+﻿open System.IO
+
+
+let validRow nums =
+    (nums |> Array.pairwise |> Array.forall (fun (a, b) -> b >= a) ||
+     nums |> Array.pairwise |> Array.forall (fun (a, b) -> b <= a)) &&
+    nums |> Array.pairwise |> Array.forall (fun (a, b) -> abs (a - b) <= 3 && a <> b)
+
+
+let canBeMadeValid (nums: int array) =
+    nums
+    |> Seq.mapi (fun i _ ->
+        Array.concat [nums.[..i-1]; nums.[i+1..]]
+    )
+    |> Seq.exists validRow
+
+let matrix (input: string array) =
+    input
+    |> Array.filter (fun row ->
+        row.Trim().Split(' ')
+        |> Array.map int
+        |> validRow)
+    |> Array.length
+
+let matrix' (input: string array) =
+    input
+    |> Array.filter (fun row ->
+        let nums = row.Trim().Split(' ') |> Array.map int
+        validRow nums || canBeMadeValid nums
+    )
+    |> Array.length
+
+
+
+let input = File.ReadAllLines "input"
+printfn "%A" (matrix' input)
