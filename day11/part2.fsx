a5fdfa9596e371821e0d553eb9f1856009d65ac5
Kasper <kl@avilo.no>
2024-12-11 19:56:13 +0100

    lazy just like me

--- day11/part2.fsx
+++ day11/part2.fsx
@@ -1,18 +1,20 @@
 open System.Collections.Generic
 
+type Lazy<'a> = System.Lazy<'a>
+
 let parseMap (input: string) =
     input.Split(' ')
     |> Seq.map int64
 
 let memoize f =
-    let cache = Dictionary<(int64 * int), bigint>()
+    let cache = Dictionary<(int64 * int), Lazy<bigint>>()
     fun x ->
         match cache.TryGetValue x with
-        | true, value -> value
+        | true, value -> value.Value
         | false, _ ->
-            let value = f x
-            cache.[x] <- value
-            value
+            let lazyValue = lazy (f x)
+            cache.[x] <- lazyValue
+            lazyValue.Value
 
 let blink stone =
     match string stone with
@@ -26,13 +28,21 @@ let rec blinks =
     fun (stone, times) ->
         match times with
         | 0 -> bigint 1
-        | n -> blink stone |> Seq.sumBy (fun st -> blinks (st, n - 1))
+        | n ->
+            blink stone
+            |> Seq.map (fun st ->
+                lazy (blinks (st, n - 1))
+            )
+            |> Seq.map (fun lazyVal -> lazyVal.Value)
+            |> Seq.sum
     |> memoize
 
 let input =
     System.IO.File.ReadAllText "input"
     |> parseMap
-
+let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 let part2 = input |> Seq.sumBy (fun st -> blinks (st, 75))
+stopWatch.Stop()
 
+printfn "%f" stopWatch.Elapsed.TotalMilliseconds
 printfn "Part 2: %A" part2
